cb7cfd6e3d6af765d35bea98999f158230d6f607
crL96 <190506813+crL96@users.noreply.github.com>
2025-05-13 16:16:39 +0200

    Add hashing to passwords before storing in db

--- src/database/queries.js
+++ src/database/queries.js
@@ -1,19 +1,22 @@
 const pool = require("./pool");
+const bcrypt = require("bcryptjs");
 
 async function getUser(username) {
     const { rows } = await pool.query("SELECT * FROM users WHERE username = $1", [username]);
     return rows[0];
 }
 
-async function addNewUser(username, fullname, hashed_password) {
+async function addNewUser(username, fullname, password) {
     // Check if username is in use
     const existingUsername = await getUser(username);
     if (existingUsername) return false;
 
+    const hashedPassword = await bcrypt.hash(password, 10)
+
     await pool.query(`
         INSERT INTO users (username, fullname, hashed_password)
         VALUES ($1, $2, $3);
-        `, [username, fullname, hashed_password]);
+        `, [username, fullname, hashedPassword]);
     console.log("Added new user to database");
     return true;
 }
