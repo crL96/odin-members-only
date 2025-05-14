76dca7455b1163caa4c19f5ddb786b8f77caf30c
crL96 <190506813+crL96@users.noreply.github.com>
2025-05-14 11:51:31 +0200

    Add server-side validation for messages

--- src/controllers/messageController.js
+++ src/controllers/messageController.js
@@ -1,18 +1,38 @@
 const db = require("../database/queries");
+const { body, validationResult } = require("express-validator");
 
 async function indexGet(req, res) {
     const messages = await db.getAllMessages();
     res.render("index", { messages: messages });
 }
 
+const validateMessage = [
+    body("title").trim()
+        .isLength({ min: 1, max: 30 }).withMessage("Title must be between 1 and 30 characters"),
+    body("message").trim()
+        .isLength({ min: 1, max: 255 }).withMessage("Title must be between 1 and 255 characters"),
+];
+
 function newMessageGet(req, res) {
     res.render("new-message-form");
 }
 
-async function newMessagePost(req, res) {
-    await db.addNewMessage(req.user.id, req.body.title, req.body.message);
-    res.redirect("/");
-}
+const newMessagePost = [
+    validateMessage,
+
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).render("sign-up-form", {
+                errors: errors.array(),
+            });
+        }
+
+        await db.addNewMessage(req.user.id, req.body.title, req.body.message);
+        res.redirect("/");
+    }
+
+];
 
 module.exports = {
     indexGet,
