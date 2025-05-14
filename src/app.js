26b1974395064f3f63aed6baeba267718a4bb4c5
crL96 <190506813+crL96@users.noreply.github.com>
2025-05-14 11:03:49 +0200

    Add comment

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ app.set("view engine", "ejs");
 app.use(session({ secret: process.env.SECRET, resave: false, saveUninitialized: false }));
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
-app.use((req, res, next) => {
+app.use((req, res, next) => { // If signed in, store user in currentUser variable so all ejs can access it
   res.locals.currentUser = req.user;
   next();
 });
